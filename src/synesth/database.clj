4b90744935330c40126c9edb3fba12d76618a36b
Mark Feltner <feltner.mj@gmail.com>
2013-03-28 19:06:40 -0500

    MongoDB interface added: supports all mongo ops; can query by any valid tag field; beginnings of music library DSL are in there

--- src/synesth/database.clj
+++ src/synesth/database.clj
@@ -1,25 +1,55 @@
 ;; database.clj
 ;; Interface to MongoDB
 (ns synesth.database
-  (:require [somnium.congomongo :as db]))
+  (:use     [somnium.congomongo :as db]
+            [somnium.congomongo.config :only [*mongo-config*]])
+  (:require [synesth.util :as util]))
 
+;; Lower-level database functions
+;; @TODO: Connect to other databases
 (def conn
   (db/make-connection "synesth-dev-db"
                       :host "127.0.0.1"
                       :port 27017))
 
-(defn drop-collections []
+(defn drop-collections [& colls]
   "Drop all the collections."
   (db/with-mongo conn
-    (db/drop-coll! "items")))
+    (dorun (db/drop-coll! colls))))
 
-(defn drop-all []
+(defn drop-db []
   "Destroy the database and all collections."
   (db/with-mongo conn
     (db/drop-database! "synesth-dev-db")))
 
+(defn db-fetch [col & options]
+  "Fetches documents from the database."
+  (let  [query  (cons col (first options))]
+    (db/with-mongo conn
+      (apply db/fetch query))))
+
+(defn library-fetch [& options]
+  "Fetches documents from the configured library."
+  (if options
+    (db-fetch :library options)
+    (db-fetch :library)))
+
+(defn by-fields [fields & options]
+  "Fetch from the configured library all documents with matching the 
+query provided in the hash-map `fields."
+  (apply library-fetch (into [:where fields] options)))
+
+(defn is-unique [& {:keys [path artist title]}]
+  (not (< 0 (by-fields  {:$or [
+                                  {:path  path}
+                                  {:$and [
+                                          {:artist  artist}
+                                          {:title   title }]}]} 
+                          :count? true))))
+
 (defn insert-audiofile [data] 
   "Insert an audiofile into the database." 
-    (db/with-mongo conn
-      (dorun 
-        (db/insert! :items data))))
+  (if (is-unique :path (:path data) :artist (:artist data) :title (:title data))
+      (db/with-mongo conn
+        (dorun
+          (db/insert! :library data)))))
